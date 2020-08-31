2382e84340489947f718563cea5788243a405c61
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-31 03:07:33 +0000

    added joke controller

---
+++ src/main/java/ratz/springframework/jokes/app/jokeapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package ratz.springframework.jokes.app.jokeapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframework.jokes.app.jokeapp.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+
+}
