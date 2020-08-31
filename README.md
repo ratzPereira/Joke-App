369d1010fe0304334090f42f4ec7176a04e8c536
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-31 02:50:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# joke-app
+ joke app using spring boot intz
---
+++ src/test/java/ratz/springframework/jokes/app/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package ratz.springframework.jokes.app.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
